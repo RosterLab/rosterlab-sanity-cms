e1d4481ee9591c8f79f580827adb6c55caf80175
icle701 <isaac@rosterlab.com>
2025-06-21 15:03:20 +1200

    FIX: fix build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 import GoogleAnalytics from "@/components/analytics/GoogleAnalytics";
 import Intercom from "@/components/analytics/Intercom";
 import Datadog from "@/components/analytics/Datadog";
